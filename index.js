1efb806abf1e7d77f8b755339f1698eaea8f48ab
Kenneth Kebaara <kkebaara@yahoo.com>
2021-08-18 04:29:35 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+document.querySelector('main').remove()
+
+let newHeader = document.createElement('h1')
+
+document.body.appendChild(newHeader);
+
+newHeader.id = 'victory';
+
+
